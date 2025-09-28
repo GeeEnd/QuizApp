6e191798b410bb6d6c9c9c9fd13edc63cdafb392
gee__een <38340742+GeeEnd@users.noreply.github.com>
2025-09-28 10:06:35 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,1 @@
-// src/config.js
-// Replace these with your real values
-export const CLIENT_ID = "146626234827-osrp9d27qm2ftb434klbi55r9mfdb9oc.apps.googleusercontent.com";
-export const API_KEY = "AIzaSyDTiexC2wn292YWCHaZJCRneEFX4TESPAQ";
-export const SHEET_ID = "1IAEq4ZDPSAJB-ccijrXEmgfTV-QkJR5v_bggj3fJhec";
-export const SCOPES = "https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.metadata.readonly";
-export const SHEET_NAME = "Questions"; // Name of the sheet with questions
-// src/config.js
-export const SHEETS_DB_URL = "https://api.sheetsdb.io/api/v1/sou6681cx9k7k/sheets";
+
